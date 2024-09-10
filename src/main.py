c90415772231fbc8604c5d4e4c23fafc8eed2a1f
KristianAskS <kristians.sin.konto@gmail.com>
2024-09-10 19:46:13 +0200

    Multithreaded

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
 import requests
 import json
 from typing import Optional, List
+from tqdm import tqdm
 import time
 import logging
 
@@ -32,7 +34,56 @@ def process_single_product(product_id: str) -> Optional[Vinmonopolprodukt]:
     return product
 
 
+def process_products_multithreaded(restart=True, workers=1) -> None:
+    product_ids = load_products()
+    products: List[Vinmonopolprodukt] = []
+    total_products = len(product_ids)
+    start_time = time.time()
+
+    if not restart:
+        with open("vinmonopol_products.json", "r", encoding="utf-8") as f:
+            products = [Vinmonopolprodukt(**p) for p in json.load(f)]
+            logger.info(f"Loaded {len(products)} products from file")
+            continuation_index = product_ids.index(products[-1].product_id)
+            product_ids = product_ids[continuation_index + 1 :]
+
+    def process_and_save(product_id: str) -> Optional[Vinmonopolprodukt]:
+        product = process_single_product(product_id)
+        if product:
+            logger.info(f"Processed product: {product.name}")
+        return product
+
+    with ThreadPoolExecutor(max_workers=workers) as executor:
+        futures = [
+            executor.submit(process_and_save, product_id) for product_id in product_ids
+        ]
+
+        for future in tqdm(
+            as_completed(futures), total=len(product_ids), desc="Processing products"
+        ):
+            product = future.result()
+            if product:
+                products.append(product)
+
+                with open("vinmonopol_products.json", "w", encoding="utf-8") as f:
+                    try:
+                        json.dump(
+                            [json.loads(p.model_dump_json()) for p in products],
+                            f,
+                            ensure_ascii=False,
+                            indent=2,
+                        )
+                    except Exception as e:
+                        logger.error(f"Failed to write to file: {e}")
+                        time.sleep(10)
+                        print(products[-1].model_dump_json())
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    logger.info(f"Time taken: {elapsed_time:.2f} seconds")
+
 def process_products(restart=True) -> None:
+    """Errors are actually shown when it's single-threaded"""
     product_ids = load_products()
     products: List[Vinmonopolprodukt] = []
     total_products = len(product_ids)
